13c8dd3c5773934053f7ebd6531f6b86340c5c24
Concentration-point <m13580614868@163.com>
2025-09-30 16:29:16 +0800

    修改时间格式

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -18,6 +18,17 @@ type templateData struct {
 	Snippets []*models.Snippet
 }
 
+// 创建一个humanDate函数，它返回一个time.Time对象的字符串表示。
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// 初始化一个template.FuncMap对象并将其存储在全局变量中。这本质上是一个
+// 字符串键控的映射，充当我们的自定义模板函数名称和函数本身之间的查找表。
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 func newTemplateCache() (map[string]*template.Template, error) {
 	// 初始化一个新映射作为缓存。
 	cache := map[string]*template.Template{}
@@ -35,11 +46,19 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		// 从完整文件路径中提取文件名（如'home.tmpl'）
 		// 并将其分配给name变量。
 		name := filepath.Base(page)
-		// 将基础模板文件解析为模板集合。
-		ts, err := template.ParseFiles("./ui/html/base.html")
+		// template.FuncMap必须在调用ParseFiles()方法之前注册到模板集合中。
+		// 这意味着我们必须使用template.New()创建一个空的模板集合，使用Funcs()方法
+		// 注册template.FuncMap，然后正常解析文件。
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
 		if err != nil {
 			return nil, err
 		}
+
+		// // 将基础模板文件解析为模板集合。
+		// ts, err := template.ParseFiles("./ui/html/base.html")
+		// if err != nil {
+		// 	return nil, err
+		// }
 
 		// *在此模板集合上*调用 ParseGlob() 以添加任何部分模板。
 		ts, err = ts.ParseGlob("./ui/html/partials/*.html")
